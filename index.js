1bf3d62699fec1abc50a47bf84208aeb66b0e6bd
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-18 14:11:04 +0100

    Javascript for matching passwords

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const form = document.getElementById('form');
 const submitBtn = document.getElementById('submit-btn');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
 
 submitBtn.addEventListener('click', function(){
     form.submit();
-
-    
 })
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('Form submitted');
+})
+
+confirmPassword.addEventListener('keyup', function(){
+    if(password.value != confirmPassword.value){
+        password.style.border = '1px solid red';
+        confirmPassword.style.border = '1px solid red';
+    } else {
+        password.style.border = '';
+        confirmPassword.style.border = '';
+    }
 })
